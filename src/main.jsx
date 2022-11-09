8af849c557d4c57c12bb5b0c8bf84d151a5eafab
matheustkaczyk <matheus111904@hotmail.com>
2022-11-09 13:35:06 -0300

    feat: structuring react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import Login from './pages/Login';
+import Management from './pages/Management';
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Management />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
