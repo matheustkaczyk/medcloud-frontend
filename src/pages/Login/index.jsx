52bdd566652c748465bc43dcf8238bc0170bf2a0
matheustkaczyk <matheus111904@hotmail.com>
2022-11-09 13:27:09 -0300

    feat: basic structure of pages

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,9 @@
+const Login = () => {
+  return (
+    <div>
+      <h1>PÁGINA DE LOGIN</h1>
+    </div>
+  )
+}
+
+export default Login;
---
+++ src/pages/Management/index.jsx
@@ -0,0 +1,9 @@
+const Management = () => {
+  return (
+    <div>
+      <h1>Management</h1>
+    </div>
+  )
+}
+
+export default Management;
