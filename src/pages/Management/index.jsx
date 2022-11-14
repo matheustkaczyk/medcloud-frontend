fec50728ec5acb9c05d5a8aec0fd3b6700925402
matheustkaczyk <matheus111904@hotmail.com>
2022-11-14 13:45:44 -0300

    feat: structuring manager page

--- src/pages/Management/index.jsx
+++ src/pages/Management/index.jsx
@@ -1,8 +1,36 @@
+import { AppBar, CssBaseline, TextField, Toolbar, Typography, } from "@mui/material";
+import { Box } from "@mui/system";
+import logo from '../../assets/medcloud.svg';
+
 const Management = () => {
   return (
-    <div>
-      <h1>Management</h1>
-    </div>
+    <>
+      <CssBaseline />
+      <AppBar position="fixed">
+        <Toolbar>
+          <Box
+            component="img"
+            sx={{
+              width: "22%",
+              padding: "1%"
+            }}
+            alt="logo"
+            src={logo}
+          />
+          <TextField
+            label="Search"
+            variant="filled"
+            margin="normal"
+            sx={{
+              width: "50%",
+              marginLeft: "10%",
+              borderRadius: "5px",
+              backgroundColor: "#ffffff"
+            }}
+          />
+        </Toolbar>
+      </AppBar>
+    </>
   )
 }
 
