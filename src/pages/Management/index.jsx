029ae612570e55468d091dbc1a476bbe15901555
matheustkaczyk <matheus111904@hotmail.com>
2022-11-15 09:36:23 -0300

    feat: creating logout and jwt validation methods

--- src/pages/Management/index.jsx
+++ src/pages/Management/index.jsx
@@ -1,11 +1,45 @@
+import { useEffect, useState } from "react";
 import { CssBaseline } from "@mui/material";
 import Header from "../../components/Header";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Management = () => {
+  const [id, setId] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+
+    return navigate("/login");
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      return handleLogout();
+    }
+
+    async function validate() {
+      await axios.post("http://localhost:3000/validate", {}, {
+        headers: {
+          Authorization: token,
+        },
+      }).then((response) => {
+        setId(response.data.id);
+      }).catch(() => {
+        handleLogout();
+      });
+    };
+
+    validate();
+  }, []);
+
   return (
     <>
       <CssBaseline />
-      <Header />
+      <Header handleLogout={handleLogout} />
     </>
   )
 }
