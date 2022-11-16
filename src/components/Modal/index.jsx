3c3e2efecf9108781e6959f2a9aa5706b969df50
matheustkaczyk <matheus111904@hotmail.com>
2022-11-16 11:33:27 -0300

    refact: making the modal to be more generic

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,55 +1,18 @@
 import React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import Modal from '@mui/material/Modal';
-import { TextField } from '@mui/material';
 
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  height: "fit-content",
-  width: "90%",
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-  display: "flex",
-  flexDirection: "column",
-};
-
-const ModalComponent = ({ open, handleClose }) => {
+const ModalComponent = (props) => {
   return (
-    <div>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <TextField
-            label="Name"
-          />
-          <TextField
-            label="Email"
-          />
-          <TextField
-            label="Description"
-          />
-          <TextField
-            label="Address"
-          />
-          <Button
-            variant="contained"
-            color="success"
-          >
-            Create
-          </ Button>
-        </Box>
-      </Modal>
-    </div>
+    <Modal
+      open={props.open}
+      onClose={props.handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      {
+        props.children
+      }
+    </Modal >
   );
 }
 
