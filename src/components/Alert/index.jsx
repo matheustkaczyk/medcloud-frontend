cfea23fbc421ed52fcb26b49458dcd9f3544e3e1
matheustkaczyk <matheus111904@hotmail.com>
2022-11-18 15:26:16 -0300

    feat: creating alert component

--- src/components/Alert/index.jsx
+++ src/components/Alert/index.jsx
@@ -1,17 +1,15 @@
 import { Alert } from "@mui/material";
 
 const AlertComponent = ({ error, message }) => {
-  const [showAlert, setShowAlert] = useState(false);
   return (
     <>
-      <Button onClick={() => setShowAlert(true)}>Show Alert</Button>
-      {showAlert && (
-        <Alert
-          severity={error ? "error" : "success"}
-          message={message}
-          onClose={() => setShowAlert(false)}
-        />
-      )}
+      {
+        error && (
+          <Alert severity="error" sx={{ width: "fit-content", position: "fixed", top: "0", right: "0", transition: "ease-in" }}>
+            {message}
+          </Alert>
+        )
+      }
     </>
   );
 }
