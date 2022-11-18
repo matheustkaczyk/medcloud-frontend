fafd829d196912429c966d874494d037f56a00c6
matheustkaczyk <matheus111904@hotmail.com>
2022-11-18 19:18:55 -0300

    feat: changing how alert components works

--- src/components/Alert/index.jsx
+++ src/components/Alert/index.jsx
@@ -1,12 +1,12 @@
 import { Alert, Fade } from "@mui/material";
 
-const AlertComponent = ({ error, message }) => {
+const AlertComponent = ({ error, message, showAlert }) => {
   return (
     <>
       {
-        error && (
+        showAlert && (
           <Fade in={error}>
-            <Alert severity="error" sx={{ width: "fit-content", position: "fixed", top: "2%", right: "2%" }}>
+            <Alert severity={error === true ? 'error' : 'success'} sx={{ width: "fit-content", position: "fixed", top: "2%", right: "2%", zIndex: 99999 }}>
               {message}
             </Alert>
           </Fade>
