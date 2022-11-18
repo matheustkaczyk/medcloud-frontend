2ec47be7fdb41c5b2f687a77dfcb1582fdf3d386
matheustkaczyk <matheus111904@hotmail.com>
2022-11-18 19:19:40 -0300

    feat: displaying alert error messages on login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -10,7 +10,7 @@ import AlertComponent from "../../components/Alert";
 
 const Login = () => {
   const [signUp, setSignup] = useState(false);
-  const [error, setError] = useState({ error: false, message: '' });
+  const [error, setError] = useState({ error: false, message: '', showAlert: false });
 
   const [signInForms, setSignInForms] = useState({
     email: '',
@@ -27,27 +27,27 @@ const Login = () => {
 
   const navigate = useNavigate();
 
-  const handleSignUp = (e) => {
+  const handleSignUp = async (e) => {
     e.preventDefault();
     const birthDateValidationRegex = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]|(?:Jan|Mar|May|Jul|Aug|Oct|Dec)))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2]|(?:Jan|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec))\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)(?:0?2|(?:Feb))\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9]|(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep))|(?:1[0-2]|(?:Oct|Nov|Dec)))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
 
     if (birthDateValidationRegex.test(signUpForms.birthDate)) {
       if (signUpForms.firstName !== '' && signUpForms.lastName !== '' && signUpForms.birthDate !== '' && signUpForms.email !== '' && signUpForms.password !== '') {
-        axios.post('http://localhost:3000/signup', signUpForms)
+        await axios.post('http://localhost:3000/signup', signUpForms)
           .then(() => {
-            alert('Account created successfully');
-            setError({ error: false, message: '' });
-            setSignup(false);
+            setError({ error: false, message: 'User sucessfully created', showAlert: true });
 
+            return setTimeout(() => {
+              setError({ error: false, message: '', showAlert: false });
+              return setSignup(false);
+            }, 3000);
           })
           .catch((err) => {
-            console.log(err)
-            setError({ error: true, message: err.response.data.error });
+            setError({ error: true, message: err.response.data.error, showAlert: true });
 
-            setTimeout(() => {
-              setError({ error: false, message: '' });
+            return setTimeout(() => {
+              setError({ error: false, message: '', showAlert: false });
             }, 3000);
-
           });
       } else {
         alert('Please fill all the fields');
@@ -63,15 +63,18 @@ const Login = () => {
     if (signInForms.email !== '' && signInForms.password !== '') {
       await axios.post('http://localhost:3000/signin', signInForms)
         .then((response) => {
-          setError({ error: false, message: '' });
+          setError({ error: false, message: '', showAlert: true });
           localStorage.setItem('token', response.data.token);
 
-          return navigate('/');
+          setTimeout(() => {
+            setError({ error: false, message: '', showAlert: false });
+            return navigate('/');
+          }, 3000);
         })
         .catch((err) => {
-          setError({ error: true, message: err.response.data.error });
+          setError({ error: true, message: err.response.data.error, showAlert: true });
           setTimeout(() => {
-            setError({ error: false, message: '' });
+            setError({ error: false, message: '', showAlert: false });
           }, 3000);
         });
     } else {
@@ -106,7 +109,7 @@ const Login = () => {
           />
         }
       </Grid>
-      <AlertComponent error={error.error} message={error.message} />
+      <AlertComponent error={error.error} message={error.message} showAlert={error.showAlert} />
     </>
   )
 }
