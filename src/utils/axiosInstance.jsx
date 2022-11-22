d8c49b11859affe241c9997010a79bae30424428
matheustkaczyk <matheus111904@hotmail.com>
2022-11-22 12:40:51 -0300

    fix: changing to ssl

--- src/utils/axiosInstance.jsx
+++ src/utils/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://18.231.120.5:3000'
+  baseURL: 'https://18.231.120.5:3000'
 });
 
 export default axiosInstance;
