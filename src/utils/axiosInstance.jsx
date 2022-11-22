3cd066da4ec43524172622a53ca838f7bed6ed35
matheustkaczyk <matheus111904@hotmail.com>
2022-11-22 12:42:01 -0300

    fix: changing ip

--- src/utils/axiosInstance.jsx
+++ src/utils/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://18.231.120.5:3000'
+  baseURL: 'https://15.228.220.238:3000'
 });
 
 export default axiosInstance;
